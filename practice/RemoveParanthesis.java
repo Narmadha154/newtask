eb9cebb7d078af8efba9d69e9a655ca2c899d38b
Narmadha154 <85552355+Narmadha154@users.noreply.github.com>
2021-08-30 17:30:55 +0530

    Add files via upload

--- practice/RemoveParanthesis.java
+++ practice/RemoveParanthesis.java
@@ -1,3 +1,4 @@
+
 package logic;
 import java.util.Scanner;
   public class RemoveParanthesis{
@@ -5,23 +6,54 @@ import java.util.Scanner;
       Scanner sc=new Scanner(System.in);
       System.out.println("Enter the string:");
       String str=sc.next();
-      char [] ch=str.toCharArray();
-      int count=0;
-      int newCount=0;
-      int m=0;
-       for(int i=0,j=str.length()-1;i<str.length()&&j>=0;i++,j--){
-         /* if(ch[i]=='('){
-              count++;
+      char [] arr=new char[50];
+      char [] newArr=new char[50];
+       int j=str.length()-1;
+       for(int i=0;i<=j-1;i++){
+          if(j==(str.length()/2)){
+             j--;
+           }
+          if(i!=j){
+            if(str.charAt(i)=='('){ 
+              arr[i]=str.charAt(i);
+            }
+            if(str.charAt(j)==')'){
+              newArr[i]=str.charAt(j);
+            }
+            if(str.charAt(i)>96&&str.charAt(i)<123||str.charAt(j)>96&&str.charAt(j)<123){
+               arr[i]=str.charAt(i);
+               newArr[i]=str.charAt(j);
+           }
+           j--;
           }
-          if(ch[j]==')'){
-            newCount++;
+           else if(i==j){
+             arr[i]=str.charAt(i);
           }
        }
-       System.out.println(count);
-        System.out.println(newCount); */
-      if(ch[i]=='('&&ch[j]==')'){
-          count++;
+      System.out.println("array");
+      for(int i=0;i<arr.length;i++){
+         System.out.print(arr[i]);
+     }
+      System.out.println("new array");
+      for(int i=0;i<newArr.length;i++){
+         System.out.print(newArr[i]);    
       }
-     else      
+      int size=arr.length+newArr.length;
+      int k=0;
+      int count=0;
+      char [] remArr=new char[size];
+      for(int i=0;i<arr.length;i++){
+         remArr[k]=arr[i];
+         k++;
+         count++;
+      }
+      for(int i=newArr.length-1;i>=0;i--){
+         remArr[count]=newArr[i];
+         count++;
+      }
+      System.out.println("balanced array");
+      for(int i=0;i<remArr.length;i++){
+         System.out.print(remArr[i]);
+     }
    }
  }
