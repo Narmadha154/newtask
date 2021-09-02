d5c269cdf4b8300706271e564a6a9c780a5e4fd5
Narmadha154 <85552355+Narmadha154@users.noreply.github.com>
2021-09-02 17:38:13 +0530

    Add files via upload

--- practice/Workers.java
+++ practice/Workers.java
@@ -6,13 +6,41 @@ public class Workers{
     Scanner sc=new Scanner(System.in);
     System.out.println("Enter the starting time:");
     double startTym=sc.nextDouble();
+    System.out.println("Whether start time am or pm:");
+    String str1=sc.next();
     System.out.println("Enter the ending time:");
     double endTym=sc.nextDouble();
+    System.out.println("Whether end time am or pm:");
+    String str2=sc.next();
     int noOfWorkers=0;
+    if(str1.equals("am")){
+      startTym=startTym;
+    }
+    else{
+     if(startTym>=1&&startTym<12){
+      startTym=12.00+startTym;
+    }
+    else{
+      startTym=startTym;
+    }
+   }
+    if(str2.equals("am")){
+       endTym=endTym;
+    }
+    else{
+     if(endTym>=1&&endTym<12){
+      endTym=12.00+endTym;
+    }
+    else{
+       endTym=endTym;
+     }
+   }
+    System.out.println("*"+endTym);
+    System.out.println("*"+startTym);
     double totalTym=endTym-startTym;
      System.out.println("*"+totalTym);
-    for(double i=totalTym;i>=0;i--){
-      if(i==0.50){
+    for(double i=totalTym;i>=0.0;i-=0.5){
+      if(i!=totalTym){
         noOfWorkers++;
       }
     }
