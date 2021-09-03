832fbe9082fb78bf995478df3db9f7a894b5ed12
Narmadha154 <85552355+Narmadha154@users.noreply.github.com>
2021-09-03 17:32:21 +0530

    Add files via upload

--- practice/RotateMatrix.java
+++ practice/RotateMatrix.java
@@ -15,21 +15,50 @@ public class RotateMatrix{
             mat[i][j]=sc.nextInt();
          }
       }
-      int temp=0;
+      int temp=0,temp1=0;
       int i=0;
-       while(i<row){
-           int j=col-1;
-           temp=mat[i][j];
+      int j=col-1;
+      temp=mat[i][j];
         for(j=col-2;j>=0;j--){
          if(j!=0){
            mat[i][j+1]=mat[i][j];
           }
           else{
-            mat[i][j+1]=mat[i][0];
+            mat[i][j+1]=mat[i][j];
           }
         }
-        i++;
-       }
+        i=row-1;
+        j=col-1;
+         temp1=mat[i][j];
+         System.out.println("*"+temp1);
+        for(i=row-1;i>0;i--){
+          if(i==row/3){
+            mat[i][j]=temp;
+          }
+         else{
+           mat[i][j]=mat[i-1][j];
+          }
+        }
+      i=row-1;
+      j=0;
+      temp=mat[i][j];
+        for(j=0;j<col-1;j++){
+         if(j!=col-2){
+           mat[i][j]=mat[i][j+1];
+          }
+          else{
+            mat[i][j]=temp1;
+          }
+        }
+        j=0;
+        for(i=0;i<row-1;i++){
+          if(i==row-2){
+            mat[i][j]=temp;
+          }
+         else{
+           mat[i][j]=mat[i+1][j];
+          }
+        }
       System.out.println("*");
       for(i=0;i<row;i++){
          for( j=0;j<col;j++){
